0878a843ab3f5792166f82097092ad9b69e9f7a3
Martin Vladic <martin.vladic@gmail.com>
2022-12-02 12:55:36 +0100

    eez-framework sample

---
+++ addon/eez-framework/sample/ui/screens.h
@@ -0,0 +1,31 @@
+#ifndef EEZ_LVGL_UI_SCREENS_H
+#define EEZ_LVGL_UI_SCREENS_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _objects_t {
+    lv_obj_t *main;
+    lv_obj_t *page1;
+} objects_t;
+
+extern objects_t objects;
+
+void create_screen_main();
+void tick_screen_main();
+
+void create_screen_page1();
+void tick_screen_page1();
+
+void create_screens();
+void tick_screen(int screen_index);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_SCREENS_H*/
